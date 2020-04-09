cc56c15299ca75abc0bde0f9495144036f2f8625
Marcin Brodowski <eltonio79@gmail.com>
2020-04-09 16:40:29 +0200

    Nowa Aneta (Tatara) - Marlin i inne

---
+++ U8G2/U8G2.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		U8G2.cpp
+ Created:	4/9/2020 2:20:53 PM
+ Author:	Asia
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "U8G2.h"
+
+
---
+++ U8G2/U8G2.h
@@ -0,0 +1,19 @@
+/*
+ Name:		U8G2.h
+ Created:	4/9/2020 2:20:53 PM
+ Author:	Asia
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _U8G2_h
+#define _U8G2_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
